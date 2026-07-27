bca28153a616f1f65df089cf7cfd9a4ef95880ec
Vigneshwaran-cse <vigneshwaran2006csc@gmail.com>
2026-07-27 23:16:01 +0530

    [1431] [Time Beats: 96.21%] [Memory Beats: 90.45%] - LeetPush

---
+++ master/1431-Kids-With-the-Greatest-Number-of-Candies.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max=0;
+        List<Boolean> Lst = new ArrayList<>();
+        for(int i=0;i<candies.length;i++)
+        {
+            if(candies[i]>=max)
+            {
+                max=candies[i];
+            }
+        }
+        for(int p=0;p<candies.length;p++)
+        {
+            int res=candies[p]+extraCandies;
+            if(res>=max)
+            {
+                Lst.add(true);
+            }
+            else
+               Lst.add(false);
+        }
+        return Lst;
+    }
+}
