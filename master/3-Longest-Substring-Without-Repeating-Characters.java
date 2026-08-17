2ebaba4e354912b617676f4b2a8d5fca1ff576dc
Vigneshwaran-cse <vigneshwaran2006csc@gmail.com>
2026-08-17 11:12:13 +0530

    [3] [Time Beats: 22.22%] [Memory Beats: 20.01%] - LeetPush

--- master/3-Longest-Substring-Without-Repeating-Characters.java
+++ master/3-Longest-Substring-Without-Repeating-Characters.java
@@ -1,20 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
-        HashSet<Character> set = new HashSet<>();
-
+        Set<Character> set = new HashSet<>();
         int left = 0;
         int maxLength = 0;
 
         for (int right = 0; right < s.length(); right++) {
-
+            // If duplicate found, shrink window from the left
             while (set.contains(s.charAt(right))) {
                 set.remove(s.charAt(left));
                 left++;
             }
 
+            // Add current character to the window
             set.add(s.charAt(right));
 
+            // Update maximum length
             maxLength = Math.max(maxLength, right - left + 1);
         }
 
