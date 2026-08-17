b5cc1194fccc21de19c3e2ee9505dcabb53856b4
Vigneshwaran-cse <vigneshwaran2006csc@gmail.com>
2026-08-17 17:04:54 +0530

    [904] [Time Beats: 81.60%] [Memory Beats: 18.18%] - LeetPush

---
+++ master/904-Fruit-Into-Baskets.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        HashMap<Integer, Integer> basket = new HashMap<>();
+        int left = 0;
+        int max_len = 0;
+
+        for (int right = 0; right < fruits.length; right++) {
+            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
+
+            // Shrink window if more than 2 fruit types
+            while (basket.size() > 2) {
+                basket.put(fruits[left], basket.get(fruits[left]) - 1);
+                if (basket.get(fruits[left]) == 0) {
+                    basket.remove(fruits[left]);
+                }
+                left++;
+            }
+
+            max_len = Math.max(max_len, right - left + 1);
+        }
+
+        return max_len;
+    }
+}
