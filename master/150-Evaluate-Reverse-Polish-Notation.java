eff17cf60f86a42806c0637ccc14988717a2f719
Vigneshwaran-cse <vigneshwaran2006csc@gmail.com>
2026-08-31 14:56:41 +0530

    [150] [Time Beats: 87.27%] [Memory Beats: 28.51%] - LeetPush

---
+++ master/150-Evaluate-Reverse-Polish-Notation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if ("+".equals(token)) {
+                stack.push(stack.pop() + stack.pop());
+            } else if ("-".equals(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            } else if ("*".equals(token)) {
+                stack.push(stack.pop() * stack.pop());
+            } else if ("/".equals(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b); // truncates toward zero
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }}
